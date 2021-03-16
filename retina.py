af5972f2e7cbb7a2e2ea5be679502262c92f0e2b
kwdaisuke <daisuke0582@gmail.com>
2021-03-16 16:12:42 +0900

    modify the prediction on terminal

--- retina.py
+++ retina.py
@@ -220,14 +220,14 @@ def select_from_folder(dataset):
         detections = inference_model.predict(input_image)
         num_detections = detections.valid_detections[0]
         class_names = [int2str(int(x)) for x in detections.nmsed_classes[0][:num_detections]]
+        predict = dict(zip(class_names, detections.nmsed_scores[0][:num_detections])) 
+        print(predict)
         label_encoder = LabelEncoder()
         label_encoder.visualize_detections(
             im,
             detections.nmsed_boxes[0][:num_detections] / ratio,
             class_names,
             detections.nmsed_scores[0][:num_detections],)
-        predict = dict(zip(class_names, detections.nmsed_scores[0][:num_detections])) 
-        print(predict)
 
 
 if __name__ == "__main__":
